f4de26e19c57568c59aa375c0d37e90425a7949f
gulnaz <gulnaz.han@yandex.ru>
2025-03-13 23:22:32 +0300

    добавили 2 упр

---
+++ Task_002/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Создаем массив из 10 целых чисел
+        int[] array = new int[10];
+        Random random = new Random();
+
+        // Заполняем массив случайными числами (например, от 1 до 100)
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(1, 101);
+        }
+
+        // Выводим массив на экран
+        Console.WriteLine("Массив: " + string.Join(", ", array));
+
+        // Подсчитываем количество чётных чисел
+        int evenCount = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] % 2 == 0)
+            {
+                evenCount++;
+            }
+        }
+
+        // Выводим результат
+        Console.WriteLine("Количество чётных чисел в массиве: " + evenCount);
+    }
+}
